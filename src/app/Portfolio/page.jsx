7d264d5de380584aff59cad9585021e5ec93efdb
NoaelSameer <142361729+NoaelSameer@users.noreply.github.com>
2025-02-19 15:20:34 -0700

    f

--- src/app/Portfolio/page.jsx
+++ src/app/Portfolio/page.jsx
@@ -6,9 +6,13 @@ export default function Home() {
   return (
     <> 
     <div className="bg-[url('https://res.cloudinary.com/dxsd4uv7m/image/upload/v1739910639/Hero_ilj7dt.jpg')] min-h-[100vh] bg-no-repeat bg-cover">
-    {/* <p>hi</p> */}
     <NavBar />
-
+    <div className="text-white mt-[10%] px-[5vw] flex-col">
+      <p className="text-[2vw] font-semibold leading-[1] px-[]">
+      <span className="text-[#FFF4FF]">My job</span> is to make sure that as our <span className="text-[#FFF44F]">customers' priorities</span> change, as the environment changes,</p>
+      <p className="text-[2vw] font-semibold leading-[1] px-[21vw]">we shift that <span className="text-[#FFF44F]">portfolio</span> of products to meet <span className="text-[#FFF44F]">them</span>"</p>
+      <p className="text-[2vw] font-semibold leading-[1] px-[34vw]">Marillyn Hewson</p>
+   </div>
     </div>
     </>
   );
