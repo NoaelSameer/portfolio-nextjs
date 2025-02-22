5014d2b737e1cb57314e946e6bbb50cca1e5b4fd
NoaelSameer <142361729+NoaelSameer@users.noreply.github.com>
2025-02-21 22:50:34 -0700

    h

--- src/app/components/navBar.jsx
+++ src/app/components/navBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 // import "../Static"
+
 function NavBar() {
   return (
     <>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,33 +2,30 @@ import dynamic from "next/dynamic";
 import Head from "next/head";
 
 const NavBar = dynamic(() => import("./components/navBar"), { ssr: true });
-const Buttons = dynamic(() => import("./components/index_buttons"), { ssr: true });
-
+import Buttons from "./components/index_buttons";
 export default function Home() {
   return (
     <>
       <Head> 
         <title>Home</title>
-        <meta
-          name="description"
-        />
+        <meta name="description" content="Portfolio of Noael Sameer, a West-Mec student specializing in website development, responsive design, and modern web technologies." />
       </Head>
-      <div className="bg-[url('https://res.cloudinary.com/dxsd4uv7m/image/upload/v1739910639/Hero_ilj7dt.jpg')] min-h-[100vh] bg-no-repeat bg-cover relative">
+      <div className="bg-[url('https://res.cloudinary.com/dxsd4uv7m/image/upload/f_auto,q_auto/v1739910639/Hero_ilj7dt.jpg')] min-h-[100vh] bg-no-repeat bg-cover relative">
         <NavBar />
         <div className="text-white mt-[7%] px-[7vw]">
-          <h1 className="text-[5vw] font-semibold leading-[1]">
+          <h1 className="text-4xl sm:text-[5vw] font-semibold leading-[1]">
             Noael<br />Sameer
           </h1>
-          <p className="text-[1vw]">
-            West-mec student specializing in website development.<br /> Expert in responsive design, user experience, and modern web technologies.
+          <p className="text-sm sm:text-[1vw]">
+            West-Mec student specializing in website development.<br /> Expert in responsive design, user experience, and modern web technologies.
           </p>
           <div className="mt-10">
             <Buttons />
           </div>
         </div>
         <img
-          src="https://res.cloudinary.com/dxsd4uv7m/image/upload/v1740201415/image0__5_-transformed_wlqaj6.webp"
-          className="w-[10vw] h-[70vh] absolute bottom-0 right-[25%] sm:w-[15vw] sm:h-[55vh]"
+          src="https://res.cloudinary.com/dxsd4uv7m/image/upload/f_auto,q_auto/v1740201415/image0__5_-transformed_wlqaj6.webp"
+          className="w-[10vw] h-[70vh] absolute bottom-0 right-[25%] hidden sm:block"
           alt="Noael Sameer"
         />
       </div>
